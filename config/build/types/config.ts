908db54162ae00ad38b6bddfb7e309d6ed326f3a
Kirill Sadovsky <car_rick@mail.ru>
2023-07-31 00:00:27 +0300

    FSD structure

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -1,18 +1,19 @@
 export type BuildMode = 'production' | 'development';
 
 export interface BuildPaths {
-  entry: string;
-  build: string;
-  html: string;
+    entry: string;
+    build: string;
+    html: string;
+    src: string;
 }
 export interface BuildOptions {
-  mode: BuildMode;
-  paths: BuildPaths;
-  isDev: boolean;
-  port: number;
+    mode: BuildMode;
+    paths: BuildPaths;
+    isDev: boolean;
+    port: number;
 }
 
 export interface BuildEnv {
-  mode: BuildMode;
-  port: number;
+    mode: BuildMode;
+    port: number;
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
-
-import { Link } from 'react-router-dom';
 import { Suspense } from 'react';
-import './styles/index.scss';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+
+import { Link, Route, Routes } from 'react-router-dom';
+
+import { classNames } from 'helpers/classNames/classNames';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+
+import { useTheme } from './providers/ThemeProvider';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -18,8 +18,8 @@ const App = () => {
             <Link to="/about">О нас</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path="/about" element={<AboutPageAsync />} />
-                    <Route path="/" element={<MainPageAsync />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/" element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
