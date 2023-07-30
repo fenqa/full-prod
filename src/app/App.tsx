3d943e07e5131c05eb2528ef85b37e7b86d3b4d9
Kirill Sadovsky <car_rick@mail.ru>
2023-07-31 00:14:07 +0300

    added routeConfig

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,8 @@
-import { Suspense } from 'react';
-
-import { Link, Route, Routes } from 'react-router-dom';
-
-import { classNames } from 'helpers/classNames/classNames';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { Link } from 'react-router-dom';
 
 import { useTheme } from './providers/ThemeProvider';
+import { classNames } from 'shared/lib/helpers/classNames/classNames';
+import { AppRouter } from './providers/router';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -16,12 +12,7 @@ const App = () => {
             <button onClick={toggleTheme}>TOGGLE THEME</button>
             <Link to="/">Главная</Link>
             <Link to="/about">О нас</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/" element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
