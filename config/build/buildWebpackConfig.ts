e2f7c2277c4e65b109f43b4673120f578e96f55b
Kirill Sadovsky <car_rick@mail.ru>
2023-07-30 18:04:40 +0300

    version fix

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -5,24 +5,22 @@ import { buildPlugins } from './buildPlugins';
 import { buildResolve } from './buildResolve';
 import buildDevServer from './buildDevServer';
 
-export function buildWebpackConfig(
-  options: BuildOptions
-): webpack.Configuration {
-  const { paths, mode, isDev } = options;
-  return {
-    mode: mode,
-    entry: paths.entry,
-    output: {
-      filename: '[name].[contenthash].js',
-      path: paths.build,
-      clean: true,
-    },
-    plugins: buildPlugins(options),
-    module: {
-      rules: buildLoaders(options),
-    },
-    resolve: buildResolve(),
-    devtool: isDev ? 'inline-source-map' : undefined,
-    devServer: isDev ? buildDevServer(options) : undefined,
-  };
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const { paths, mode, isDev } = options;
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolve(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+    };
 }
